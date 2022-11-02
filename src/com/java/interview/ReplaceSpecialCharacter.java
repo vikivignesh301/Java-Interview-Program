5a0d95214c1ed2c2fbbfdcb40ce2764908e90b4e
vikivignesh301 <vikivignesh.1301@gmail.com>
2022-11-02 17:33:00 +0530

    first commit

--- src/com/java/interview/ReplaceSpecialCharacter.java
+++ src/com/java/interview/ReplaceSpecialCharacter.java
@@ -8,8 +8,16 @@ public class ReplaceSpecialCharacter {
 		String replace = s.replaceAll("[^a-zA-Z0-9]", " ");
 		System.out.println(replace);
 	}
+	
+	public static void subMethod1() {
+		
+		String s = "selenium123";
+		String re = s.replaceAll("[^a-zA-Z]", "");
+		System.out.println(re);
+	}
 	public static void main(String[] args) {
 		subMethod();
+		subMethod1();
 	}
 	
 	
